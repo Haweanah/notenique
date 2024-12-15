8fbcbba0139eb76d95154c33963dc3f5e8b52240
Haweanah <ahauwa48@yahoo.com>
2024-12-15 20:22:10 +0100

    added templates

--- notenique.py
+++ notenique.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+  {
+    'author': 'Nana Hauwa',
+    'title': 'My First Blog Post',
+    'content': 'This is my first ever post on Notenique app and I hope to make the best of it',
+    'date_posted': 'Jan 9, 1999'
+  },
+
+  {
+    'author': 'Fola Adeola',
+    'title': 'My Second Blog Post',
+    'content': 'This is my second post on Notenique app and I hope you find it interesting',
+    'date_posted': 'Jan 28, 1999'
+  }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return '<h1>Home Page Notenique!</h1>'
+  return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-  return '<h1>About Page Notenique!</h1>'
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
