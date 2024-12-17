bce26b6b9af673bbcbcfc501b276c2c207477c5c
Haweanah <ahauwa48@yahoo.com>
2024-12-17 18:38:18 +0100

    Creating Database

--- notenique.py
+++ notenique.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'aaa95e726010ac65ea9a882fa1e0bcd3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(20), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(70), nullable=False)
 
 posts = [
   {
@@ -36,9 +46,15 @@ def register():
     flash(f'Account successfully created for {form.username.data}!', 'success')
     return redirect(url_for('home'))
   return render_template('register.html', title='Register', form=form)
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+      flash('Logged in successfully!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Failed to Login. Please check your email and password', 'danger')
   return render_template('login.html', title='Register', form=form)
 
 if __name__ == '__main__':
