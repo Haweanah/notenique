e58e7448b36582094496d1192811bf5275975a87
Haweanah <ahauwa48@yahoo.com>
2024-12-18 16:43:09 +0100

    Configured site-db file

--- notenique/__init__.py
+++ notenique/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aaa95e726010ac65ea9a882fa1e0bcd3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get the project directory
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "site.db")}'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
