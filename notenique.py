54e31f61b2b32e87591ae68eeadd0d3d3aa93598
Haweanah <ahauwa48@yahoo.com>
2024-12-18 07:38:02 +0100

    added Dashboard template

--- notenique.py
+++ notenique.py
@@ -14,20 +14,24 @@ class User(db.Model):
   email = db.Column(db.String(20), unique=True, nullable=False)
   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
   password = db.Column(db.String(70), nullable=False)
+  notes = db.relationship('Note', backref='author', lazy=True)
 
   def __repr__(self):
     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Note(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.Datetime, nullable=False, default=datetime.now(timezone.utc))
+  title = db.Column(db.String(100), nullable=False)
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
   content = db.Column(db.Text, nullable=False, )
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
   def __repr__(self):
-    return f"Post('{self.title}', '{self.date_posted}')"
+    return f"Note('{self.title}', '{self.date_posted}')"
 
 
-posts = [
+notes = [
   {
     'author': 'Nana Hauwa',
     'title': 'My First Blog Post',
@@ -46,11 +50,15 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template('home.html', posts=posts)
+  return render_template('home.html', notes=notes)
 
 @app.route("/about")
 def about():
   return render_template('about.html', title='About')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html', notes=notes)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -59,16 +67,19 @@ def register():
     flash(f'Account successfully created for {form.username.data}!', 'success')
     return redirect(url_for('home'))
   return render_template('register.html', title='Register', form=form)
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit():
     if form.email.data == 'admin@blog.com' and form.password.data == 'password':
       flash('Logged in successfully!', 'success')
-      return redirect(url_for('home'))
+      return redirect(url_for('dashboard'))
     else:
       flash('Failed to Login. Please check your email and password', 'danger')
   return render_template('login.html', title='Register', form=form)
+
+  
 
 if __name__ == '__main__':
   app.run(debug=True)
