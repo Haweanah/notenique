3a529f992cf62c23d6810b648b618311828477e0
Haweanah <ahauwa48@yahoo.com>
2024-12-20 12:32:35 +0100

    more authentication

--- notenique/__init__.py
+++ notenique/__init__.py
@@ -2,14 +2,16 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aaa95e726010ac65ea9a882fa1e0bcd3'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get the project directory
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "site.db")}'
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from notenique import routes
--- notenique/forms.py
+++ notenique/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from notenique.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), Length(min=6, max=15), EqualTo('password')]) 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken')
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already taken')
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
