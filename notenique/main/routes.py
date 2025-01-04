a2c6ffab11fca27b24dc7a5f935d0a4b920aa03c
Haweanah <ahauwa48@yahoo.com>
2025-01-04 13:19:56 +0100

    Creating Routes Blueprint

---
+++ notenique/main/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+@main.route('/home', methods=['GET'])
+def home():
+    page = request.args.get('page', 1, type=int)  # Get the current page number from the request
+    per_page = 5
+    if current_user.is_authenticated:
+        notes = Note.query.filter_by(author=current_user).paginate(page=page, per_page=per_page, error_out=False)
+
+    else:
+        notes = Note.query.paginate(page=page, per_page=5)
+    return render_template('home.html', notes=notes)
+
+@main.route("/about")
+def about():
+  return render_template('about.html', title='About')
---
+++ notenique/posts/routes.py
@@ -0,0 +1,49 @@
+from flask import Blueprint
+
+posts = Blueprint('posts', __name__)
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_note():
+    form = NoteForm()
+    
+    if form.validate_on_submit():
+        # Capture the HTML content from Quill editor
+        content = form.content.data  # This is the content passed from the form (which will be Quill content)
+
+        # Save the new note with the content (HTML)
+        note = Note(title=form.title.data, content=content, author=current_user)
+        db.session.add(note)
+        db.session.commit()
+
+        flash('Your note has been created!', 'success')
+        return redirect(url_for('home'))
+    
+    return render_template('create_note.html', title='New Note', form=form, legend='New Note')
+
+@posts.route("/note/<int:note_id>")
+def note(note_id):
+  note = Note.query.get_or_404(note_id)
+  return render_template('note.html', title=note.title, note=note)
+
+@posts.route("/note/<int:note_id>/edit", methods=['GET', 'POST'])
+@login_required
+def edit_note(note_id):
+  note = Note.query.get_or_404(note_id)
+  if note.author != current_user:
+    abort(403)
+  form = NoteForm()
+  if form.validate_on_submit():
+    note.title = form.title.data
+    note.content = form.content.data
+    db.session.commit()
+    flash('Your note has been edited', 'success')
+    return redirect(url_for('note', note_id=note.id))
+  elif request.method == 'GET':
+    form.title.data = note.title
+    form.content.data = note.content
+  return render_template('create_note.html', title='Edit Note', form=form, legend='Edit Note')
+
+@posts.route("/note/<int:note_id>/delete", methods=['POST'])
+@login_required
+def delete_note(note_id):
