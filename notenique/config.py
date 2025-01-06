5f0dcf47e73c20ebd813e7f2216afa2f16020afa
Haweanah <ahauwa48@yahoo.com>
2025-01-06 09:07:09 +0100

    Resolving environment variables

--- notenique/config.py
+++ notenique/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 class Config:
     """Base configuration class for the application."""
@@ -9,15 +13,13 @@ class Config:
     # Base directory for project files
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    # Database URI: Read from environment variable or use SQLite as fallback
+    # Database URI: Read from environment variable or default to SQLite
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'SQLALCHEMY_DATABASE_URI',
-        f'sqlite:///{os.path.join(BASE_DIR, "site.db")}'
+        f"sqlite:///{os.path.join(BASE_DIR, 'site.db')}"
     )
 
-    # Optional: Additional configurations for SQLAlchemy
+    # Optional: Additional configurations
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Add other configuration variables as needed
     DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't', 'yes']
     TESTING = os.getenv('TESTING', 'False').lower() in ['true', '1', 't', 'yes']
--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from notenique import create_app 
+from dotenv import load_dotenv
+import os
+from notenique import create_app
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Create the Flask application instance
 app = create_app()
 
+# Run the application
 if __name__ == '__main__':
-  app.run(debug=True)
+    # Debug mode is controlled by the DEBUG environment variable
+    debug_mode = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't', 'yes']
+    app.run(debug=debug_mode)
