fb281b0f0a05f553de667c10d7498b285a1fdd68
Haweanah <ahauwa48@yahoo.com>
2026-05-22 10:25:19 +0100

    Create secret key

--- notenique/config.py
+++ notenique/config.py
@@ -6,10 +6,15 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-    'DATABASE_URL',
-    os.getenv('SQLALCHEMY_DATABASE_URI')
-)
+    database_url = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = (
+        database_url.replace(
+            "postgresql://",
+            "postgresql+psycopg2://"
+        )
+        if database_url else None
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv('DEBUG') == 'True'
